239f292221656de1be3010b9a4e033051d4c8a9a
Bioinfowangm <mengwang55@gmail.com>
2022-05-31 14:49:53 -0700

    Update run_CNVkit_WGS.sh

--- Pipeline_Scripts/run_CNVkit_WGS.sh
+++ Pipeline_Scripts/run_CNVkit_WGS.sh
@@ -2,12 +2,13 @@
 
 #Key variables(need to update for each patient)
 REFERENCE_GENOME="genome.fa"  # Genome Reference (hg19)
-GERMLINE_BAM_base=" "     # prefix of normal sample(not including '.bam')
-GERMLINE_BAM=${GERMLINE_BAM_base}.bam
 TUMOR_BAM_base=" "        # prefix of tumor bam sample(not including '.bam')
 TUMOR_BAM=${TUMOR_BAM_base}.bam
+Germline_vcf=" " # Germline mutations in tumor and normal samples, can be from freebayes or other sources
 Purity=" "
 Diploid_logR=" "
+Target_Interval=" "
+Gender="x/y" # 'x': female; 'y': male
 
 cnvkit.py coverage -p 4 $GERMLINE_BAM ../Intervals/wgs_Target.bed -o ${GERMLINE_BAM_base}.targetcoverage.cnn
 cnvkit.py coverage -p 4 $TUMOR_BAM ../Intervals/wgs_Target.bed -o ${TUMOR_BAM_base}.targetcoverage.cnn
@@ -24,6 +25,6 @@ cnvkit.py segment ${TUMOR_BAM_base}.cnr -t 1e-09 --drop-low-coverage -o ${TUMOR_
 cnvkit.py scatter ${TUMOR_BAM_base}.cnr -s ${TUMOR_BAM_base}.cns -o ${TUMOR_BAM_base}-scatter.pdf
 cnvkit.py diagram ${TUMOR_BAM_base}.cnr -s ${TUMOR_BAM_base}.cns -o ${TUMOR_BAM_base}-diagram.pdf
 
-# Re-center
-cnvkit.py call ${TUMOR_BAM_base}.cnr  --purity $Purity --center-at $Diploid_logR -m clonal -x x -o ${TUMOR_BAM_base}.call.cnr
-cnvkit.py call ${TUMOR_BAM_base}.cns  --purity $Purity --center-at $Diploid_logR -m clonal -x x -o ${TUMOR_BAM_base}.call.cns
+# Re-center (optional)
+cnvkit.py call ${TUMOR_BAM_base}.cnr  --purity $Purity --center-at $Diploid_logR -m clonal -x $Gender -o ${TUMOR_BAM_base}.call.cnr
+cnvkit.py call ${TUMOR_BAM_base}.cns  --purity $Purity --center-at $Diploid_logR -m clonal -x $Gender -v $Germline_vcf -o ${TUMOR_BAM_base}.call.cns
