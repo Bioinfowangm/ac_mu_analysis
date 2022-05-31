aaf6902c34a692f83f1665d6c401e69b1f8c8e86
Bioinfowangm <mengwang55@gmail.com>
2022-05-31 15:13:52 -0700

    Create run_MSIsensor.sh

---
+++ Pipeline/run_MSIsensor.sh
@@ -0,0 +1,19 @@
+# v0.2
+#Key variables(need to update for each patient)
+REFERENCE_GENOME="genome.fa"  # Genome Reference (hg19)
+GERMLINE_BAM_base=" "     # prefix of normal sample(not including '.bam')
+GERMLINE_BAM=${GERMLINE_BAM_base}.bam
+TUMOR_BAM_base=" "        # prefix of tumor bam sample(not including '.bam')
+TUMOR_BAM=${TUMOR_BAM_base}.bam
+Patient_Name=" "     # name of the patient
+
+#Step1: Scan genome
+msisensor scan $REFERENCE_GENOME -o hg19_msisensor.txt
+
+#Step2: run for each patient
+msisensor msi \
+    -d hg19_msisensor.txt \
+    -t $TUMOR_BAM \
+    -n $GERMLINE_BAM \
+    -e ../Intervals/hg19_RefSeq_CDSunique_add10bp.bed \
+    -o ${Patient_Name}.MSIsensor
