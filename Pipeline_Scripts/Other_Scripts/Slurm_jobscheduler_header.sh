52f2e82c757677b8e5b730b40b322612e6240640
Bioinfowangm <mengwang55@gmail.com>
2022-05-30 17:39:35 -0700

    Create Slurm_jobscheduler_header.sh

---
+++ Pipeline_Scripts/Other_Scripts/Slurm_jobscheduler_header.sh
@@ -0,0 +1,11 @@
+# Note: below is a header script for a Slurm linux job schedular
+# (4 threads on 1 node; memory 30GB; time limit of running is 48 hours)
+# if there submit multiple very similar jobs, consider using array
+
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=30G
+#SBATCH --time=48:00:00
+#SBATCH --array=0-5
+#SBATCH --output=%x-%A-%a.out
