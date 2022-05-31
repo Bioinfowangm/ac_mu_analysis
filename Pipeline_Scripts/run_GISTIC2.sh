3410205ddcbd639dca4d8d23b47fd281d8f67bf7
Bioinfowangm <mengwang55@gmail.com>
2022-05-31 15:14:26 -0700

    Delete run_GISTIC2.sh

--- Pipeline_Scripts/run_GISTIC2.sh
+++
@@ -1,8 +0,0 @@
-#Key variables(need to update for each patient)
-Base_dir=" "  # output folder
-Segmentation_fn=" " # file with segmentation data of all samples
-
-gistic2 \
-    -b $Base_dir \
-    -seg $Segmentation_fn \
-    -refgene ../Intervals/hg19.mat
